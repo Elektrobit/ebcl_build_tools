18177659ee1e1f558d08bb89c3a8dc61453b6e97
Tom Irgang <thomas@irgang.eu>
2024-12-17 15:34:34 +0000

    developer user in container

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+ln -sf /build/venv .venv
+
+UID=$(id -u)
+if [ $UID -ne 0 ]; then
+    echo "Fixing permissions of /build"
+    sudo chown -R $(id -un):$(id -gn) /build
+fi
+
 # install the build tools
 source /build/venv/bin/activate && pip install -e .
 
@@ -7,4 +15,3 @@ source /build/venv/bin/activate && pip install -e .
 source /build/venv/bin/activate && pip install -r dev-requirements.txt
 
 ln -sf /build build
-ln -sf /build/venv .venv
