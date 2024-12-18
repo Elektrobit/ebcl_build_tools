cf0b970cc4e6293a5f77a7f0ff8513ea4e5cb630
Tom Irgang <thomas@irgang.eu>
2024-12-18 12:50:53 +0000

    comment dev container

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+# Link the venv, so that the WolfiesHorizon.python-auto-venv
+# VS Code plugin will use it by default.
 ln -sf /build/venv .venv
 
 UID=$(id -u)
 if [ $UID -ne 0 ]; then
+    # Change ownership of the venv and the installed tool
+    # to allow installation of further Python packages and
+    # live hacking of the tools.
     echo "Fixing permissions of /build"
     sudo chown -R $(id -un):$(id -gn) /build
 fi
 
-# install the build tools
+# Install the build tools from the local repository.
 source /build/venv/bin/activate && pip install -e .
 
-# install dev dependencies
+# Install the development dependencies.
 source /build/venv/bin/activate && pip install -r dev-requirements.txt
 
+# Link the build folder to the workspace, so that the tools
+# can be inspected using the VS Code file browser.
 ln -sf /build build
