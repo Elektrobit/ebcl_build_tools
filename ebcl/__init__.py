099525ec27f245b231e98d4cef18005e5ac7ac67
Tom Irgang <thomas@irgang.eu>
2024-12-19 13:32:46 +0000

    release 2.0.0

--- ebcl/__init__.py
+++ ebcl/__init__.py
@@ -1,5 +1,5 @@
 """ Build tools for embedded Linux images. """
-__version__ = "1.3.5"
+__version__ = "2.0.0"
 __dependencies__ = "sudo debootstrap bash apt coreutils mount rsync tar gnupg wget " \
     "cpio findutils zstd file fakeroot fakechroot"
 __ebcl_repo_key__ = "https://linux.elektrobit.com/eb-corbos-linux/ebcl_1.0_key.pub"
