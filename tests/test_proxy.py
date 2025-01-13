8fe09e77f2adae360ae75dcc999d633587d8c050
Joerg Vehlow <joerg.vehlow@aox.de>
2025-01-13 09:41:07 +0000

    test_proxy: use a clean instance of Proxy for every test
    This removes dependencies between tests.
    Also change ebcl version for test_find_bootstrap_package to 1.4,
    before bootstrap-root-ubuntu-jammy was not architecture dependent

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -13,10 +13,8 @@ from ebcl.common.version import parse_depends
 class TestProxy:
     """ Unit tests for the EBcL apt proxy. """
 
-    proxy: Proxy
-
     @property
-    def ubuntu_jammy_repo(self):
+    def ubuntu_jammy_repo(self) -> AptDebRepo:
         return AptDebRepo(
             url='http://archive.ubuntu.com/ubuntu',
             dist='jammy',
@@ -24,18 +22,15 @@ class TestProxy:
             arch=CpuArch.AMD64
         )
 
-    @classmethod
-    def setup_class(cls):
-        """ Prepare proxy. """
-        cls.proxy = Proxy()
-
-    def test_init(self):
+    def test_init(self) -> None:
         """ Test proxy initalization. """
-        assert self.proxy.apts == []
-        assert self.proxy.cache is not None
+        proxy = Proxy()
+        assert proxy.apts == []
+        assert proxy.cache is not None
 
-    def test_apt_repos(self):
+    def test_apt_repos(self) -> None:
         """ Test apt repo handling. """
+        proxy = Proxy()
         a = Apt(
             AptDebRepo(
                 url='ports.ubuntu.com/ubuntu-ports',
@@ -53,39 +48,39 @@ class TestProxy:
             )
         )
 
-        assert len(self.proxy.apts) == 0
+        assert len(proxy.apts) == 0
 
-        res = self.proxy.add_apt(Apt(self.ubuntu_jammy_repo))
+        res = proxy.add_apt(Apt(self.ubuntu_jammy_repo))
         assert res
-        assert len(self.proxy.apts) == 1
+        assert len(proxy.apts) == 1
 
-        res = self.proxy.add_apt(Apt(self.ubuntu_jammy_repo))
+        res = proxy.add_apt(Apt(self.ubuntu_jammy_repo))
         assert not res
-        assert len(self.proxy.apts) == 1
+        assert len(proxy.apts) == 1
 
-        res = self.proxy.add_apt(a)
+        res = proxy.add_apt(a)
         assert res
-        assert len(self.proxy.apts) == 2
+        assert len(proxy.apts) == 2
 
-        res = self.proxy.add_apt(b)
+        res = proxy.add_apt(b)
         assert res
-        assert len(self.proxy.apts) == 3
+        assert len(proxy.apts) == 3
 
-        res = self.proxy.remove_apt(a)
+        res = proxy.remove_apt(a)
         assert res
-        assert len(self.proxy.apts) == 2
+        assert len(proxy.apts) == 2
 
-        res = self.proxy.remove_apt(a)
+        res = proxy.remove_apt(a)
         assert not res
-        assert len(self.proxy.apts) == 2
+        assert len(proxy.apts) == 2
 
-    def test_find_package_busybox(self):
+    def test_find_package_busybox(self) -> None:
         """ Test that busybox-static package is found. """
-        self.proxy.add_apt(Apt(self.ubuntu_jammy_repo))
+        proxy = Proxy([Apt(self.ubuntu_jammy_repo)])
 
         vds = parse_depends('busybox-static', CpuArch.AMD64)
         assert vds
-        p = self.proxy.find_package(vds[0])
+        p = proxy.find_package(vds[0])
         assert p is not None
         assert p.name == 'busybox-static'
         assert p.arch == CpuArch.AMD64
@@ -99,62 +94,61 @@ class TestProxy:
             )
         )
 
-        self.proxy.add_apt(a)
+        proxy.add_apt(a)
 
         vds = parse_depends('busybox-static', CpuArch.ARM64)
         assert vds
-        p = self.proxy.find_package(vds[0])
+        p = proxy.find_package(vds[0])
         assert p is not None
         assert p.name == 'busybox-static'
         assert p.arch == CpuArch.ARM64
 
-    def test_find_linux_image_generic(self):
-        """ Test that busybox-static package is found. """
-        self.proxy.add_apt(Apt(self.ubuntu_jammy_repo))
+    def test_find_linux_image_generic(self) -> None:
+        proxy = Proxy([Apt(self.ubuntu_jammy_repo)])
 
         vds = parse_depends('linux-image-generic', CpuArch.AMD64)
         assert vds
-        p = self.proxy.find_package(vds[0])
+        p = proxy.find_package(vds[0])
         assert p is not None
         assert p.name == 'linux-image-generic'
         assert p.arch == CpuArch.AMD64
         assert p.depends
 
-    def test_find_bootstrap_package(self):
+    def test_find_bootstrap_package(self) -> None:
         """ Test that bootstrap-root-ubuntu-jammy package is found. """
-        self.proxy.add_apt(Apt.ebcl_apt(CpuArch.AMD64, '1.2'))
+        proxy = Proxy([Apt.ebcl_apt(CpuArch.AMD64, '1.4')])
 
         vds = parse_depends('bootstrap-root-ubuntu-jammy', CpuArch.AMD64)
         assert vds
-        p = self.proxy.find_package(vds[0])
+        p = proxy.find_package(vds[0])
         assert p is not None
         assert p.name == 'bootstrap-root-ubuntu-jammy'
         assert p.arch == CpuArch.AMD64
 
-        self.proxy.add_apt(Apt.ebcl_apt(CpuArch.ARM64, '1.2'))
+        proxy = Proxy([Apt.ebcl_apt(CpuArch.ARM64, '1.4')])
 
         vds = parse_depends('bootstrap-root-ubuntu-jammy', CpuArch.ARM64)
         assert vds
-        p = self.proxy.find_package(vds[0])
+        p = proxy.find_package(vds[0])
         assert p is not None
         assert p.name == 'bootstrap-root-ubuntu-jammy'
         assert p.arch == CpuArch.ARM64
 
-    def test_find_not_existing(self):
+    def test_find_not_existing(self) -> None:
         """ Test that tries to find a non-existing package. """
         vds = parse_depends('some-not-existing-package', CpuArch.AMD64)
         assert vds
-        p = self.proxy.find_package(vds[0])
+        p = Proxy().find_package(vds[0])
         assert p is None
 
     @pytest.mark.requires_download
-    def test_download_and_extract_linux_image(self):
+    def test_download_and_extract_linux_image(self) -> None:
         """ Extract data content of multiple debs. """
-        self.proxy.add_apt(Apt(self.ubuntu_jammy_repo))
+        proxy = Proxy([Apt(self.ubuntu_jammy_repo)])
 
         vds = parse_depends('linux-image-generic', CpuArch.AMD64)
         assert vds
-        (debs, content, missing) = self.proxy.download_deb_packages(vds)
+        (debs, content, missing) = proxy.download_deb_packages(vds)
 
         assert not missing
 
