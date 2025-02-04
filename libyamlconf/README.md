437156966444176fd6f9e17bc82119e770a4d102
Tom Irgang <thomas@irgang.eu>
2025-02-04 18:11:27 +0100

    Revert "copy config code"
    This reverts commit 02e52fb579f853d234d40ecce8f4c1c68522635e.

--- libyamlconf/README.md
+++
@@ -1,3 +0,0 @@
-# Libyamlconf
-
-Libyamlconf provides a generic yaml config parser supporting hierarchical configuration.
--- libyamlconf/src/libyamlconf/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Libyamlconf provides a generic yaml config parser supporting hierarchical configuration.
-"""
-__version__ = "0.1.0"
