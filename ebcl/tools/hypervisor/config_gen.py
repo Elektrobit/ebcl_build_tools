65c8379c54e97156b699d92d9ace2b084430f57c
Joerg Vehlow <joerg.vehlow@aox.de>
2025-02-11 08:09:59 +0000

    hypervisor: add ability to unpack hypervisor specialization package
    This allows automatic download and unpacking of a debin package with
    the hypervisor specialization without any external tooling

--- ebcl/tools/hypervisor/config_gen.py
+++ ebcl/tools/hypervisor/config_gen.py
@@ -1,13 +1,18 @@
 import argparse
 import logging
 import os
+
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
 import jinja2
 import yaml
 
 from ebcl.common import init_logging, log_exception
+from ebcl.common.config import Config
 from ebcl.common.files import resolve_file
+from ebcl.common.version import VersionDepends
+from ebcl.tools.hypervisor.model_gen import ConfigError
 
 from .schema_loader import BaseModel, FileReadProtocol, Schema, merge_dict
 
@@ -90,6 +95,64 @@ class HvFileGenerator:
                 outpath.write_text(template.read_text("utf-8"))
 
 
+class SpecializationUnpacker:
+    """
+    Unpacks a debian package with hypervisor specialization
+
+    The property directory returns the path to the files on the local filesystem.
+    They are removed when this object is deleted.
+
+    The path is looked up automatically searching for schema.yaml. It can also be
+    specified in the constructor (path).
+    """
+    _tmp_dir: TemporaryDirectory
+    _config_dir: Path
+
+    def __init__(self, package_name: str, config_file: Path, path: str | None = None) -> None:
+        self._tmp_dir = TemporaryDirectory()
+        self._config_dir = Path(self._tmp_dir.name)
+
+        config = Config(str(config_file), self._tmp_dir.name)
+
+        package = config.proxy.find_package(
+            VersionDepends(package_name, None, None, None, config.arch)
+        )
+        if not package:
+            raise ConfigError(f"Cannot find package {package_name}")
+        package = config.proxy.download_package(config.arch, package)
+        if not package:  # pragma: no cover (should never happen)
+            raise ConfigError(f"Cannot download package {package_name}")
+
+        print(package.local_file)
+        print(package.extract(self._tmp_dir.name, None, use_sudo=False))
+
+        os.system(f"find {self._tmp_dir.name}")
+
+        if path:
+            self._config_dir = self._config_dir / path
+        else:
+            self._config_dir = self.find_schema()
+
+    def find_schema(self) -> Path:
+        """Find a single instance of schema.yaml in the archive"""
+        directories: list[Path] = []
+        for root, _, files in os.walk(self._tmp_dir.name):
+            if "schema.yaml" in files:
+                directories.append(Path(root))
+
+        if len(directories) != 1:
+            raise ConfigError(f"Expected exactly one schema.yaml in specialization package, found {len(directories)}.")
+        return directories[0]
+
+    def __del__(self) -> None:
+        self._tmp_dir.cleanup()
+
+    @property
+    def directory(self) -> Path:
+        """Path to the extension directory on the local filesystem"""
+        return self._config_dir
+
+
 @log_exception(call_exit=True)
 def main() -> None:
     """ Main entrypoint of EBcL hypervisor generator. """
@@ -103,12 +166,23 @@ def main() -> None:
         description='Create the config files for the hypervisor')
     parser.add_argument('-s', '--specialization', type=Path,
                         help='Path to hypervisor specialization directory')
+    parser.add_argument('-p', '--specialization-package', type=str,
+                        help='Name of a debian package that contains the hypervisor specialization')
+    parser.add_argument('--specialization-path', type=str,
+                        help='Path to specialization in package')
+    parser.add_argument('-r', '--repo-config', type=Path,
+                        help='Path to a config file with a repository containing the hypervisor specialization')
     parser.add_argument('config_file', type=Path,
                         help='Path to the YAML configuration file')
     parser.add_argument('output', type=Path,
                         help='Path to the output directory')
     args = parser.parse_args()
 
+    if args.specialization_package:
+        if not args.repo_config or not args.repo_config.exists():
+            parser.error("If a SPECIALIZATION_PACKAGE is specified a REPO_CONFIG must be specified as well")
+        unpacker = SpecializationUnpacker(args.specialization_package, args.repo_config, args.specialization_path)
+        args.specialization = unpacker.directory
     generator = HvFileGenerator(args.config_file, args.output, args.specialization)
     generator.create_files()
 
--- tests/hypervisor/test_config_gen.py
+++ tests/hypervisor/test_config_gen.py
@@ -3,9 +3,21 @@ import pytest
 
 from pathlib import Path
 
-from ebcl.tools.hypervisor.config_gen import BaseResolver, main as hv_main
+import yaml
+
+from ebcl.common.types.cpu_arch import CpuArch
+from ebcl.tools.hypervisor.config_gen import BaseResolver, SpecializationUnpacker, main as hv_main
+from ebcl.tools.hypervisor.model_gen import ConfigError
 
 test_data = Path(__file__).parent / "data"
+
+
+def compare_directories(actual: Path, expected: Path) -> None:
+    created_files = sorted(actual.iterdir())
+    expected_files = sorted(expected.iterdir())
+    assert list(map(lambda x: x.name, created_files)) == list(map(lambda x: x.name, expected_files))
+    for act, exp in zip(created_files, expected_files):
+        assert act.read_text() == exp.read_text(), f"{act.name} differs"
 
 
 class TestBaseResolver:
@@ -29,6 +41,76 @@ class TestBaseResolver:
             BaseResolver().load("missing_include.yaml", test_data)
 
 
+class TestSpecializationUnpacker:
+    config: Path
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, tmp_path: Path) -> None:
+        self.config = tmp_path / "config.yaml"
+        with self.config.open("w") as f:
+            yaml.dump(
+                {
+                    'arch': str(CpuArch.ARM64),
+                    'apt_repos': [
+                        {
+                            'apt_repo': "file://" + str(test_data / "test_packages"),
+                            'directory': '.'
+                        }
+                    ]
+                },
+                f
+            )
+
+    def test_missing(self) -> None:
+        with pytest.raises(ConfigError, match="^Cannot find package nonexisting-package$"):
+            SpecializationUnpacker("nonexisting-package", self.config)
+
+    def test_empty(self) -> None:
+        with pytest.raises(ConfigError, match="^Expected exactly one schema.yaml in specialization package, found 0.$"):
+            SpecializationUnpacker("test-empty", self.config)
+
+    def test_simple(self) -> None:
+        unpacker = SpecializationUnpacker("test-simple", self.config)
+        assert unpacker.directory.relative_to(unpacker._tmp_dir.name) == Path("a/b")
+
+        path = Path(unpacker._tmp_dir.name)
+        del unpacker
+        assert path.exists() is False
+
+    def test_multiple(self) -> None:
+        with pytest.raises(ConfigError, match="^Expected exactly one schema.yaml in specialization package, found 2.$"):
+            SpecializationUnpacker("test-multiple", self.config)
+
+    def test_manual(self) -> None:
+        unpacker = SpecializationUnpacker("test-multiple", self.config, "a/b")
+        assert unpacker.directory.relative_to(unpacker._tmp_dir.name) == Path("a/b")
+
+    def test_cli(self, tmp_path: Path, capsys: pytest.CaptureFixture) -> None:
+        sys.argv = [
+            "hypervisor_config",
+            "--specialization-package", "test-ebclfsa",
+            str(test_data / "examples" / "qemu_ebclfsa" / "config.yaml"),
+            str(tmp_path / "out")
+        ]
+        # Missing repo config leads to system exit
+        with pytest.raises(SystemExit):
+            hv_main()
+        assert capsys.readouterr().err.endswith(
+            "If a SPECIALIZATION_PACKAGE is specified a REPO_CONFIG must be specified as well\n"
+        )
+
+        sys.argv = [
+            "hypervisor_config",
+            "--specialization-package", "test-ebclfsa",
+            "--repo-config", str(self.config),
+            str(test_data / "examples" / "qemu_ebclfsa" / "config.yaml"),
+            str(tmp_path / "out")
+        ]
+        hv_main()
+
+        compare_directories(tmp_path / "out", test_data / "examples" / "qemu_ebclfsa" / "expected")
+
+
 real_examples = list((test_data / "examples").iterdir())
 
 
@@ -49,11 +131,7 @@ def test_examples(path: Path, tmp_path: Path) -> None:
         str(tmp_path)
     ]
     if extension_dir.is_dir():
-        sys.argv += ["-s", str(extension_dir)]
+        sys.argv += ["--specialization", str(extension_dir)]
     hv_main()
 
-    created_files = sorted(tmp_path.iterdir())
-    expected_files = sorted(expected_dir.iterdir())
-    assert list(map(lambda x: x.name, created_files)) == list(map(lambda x: x.name, expected_files))
-    for act, exp in zip(created_files, expected_files):
-        assert act.read_text() == exp.read_text(), f"{act.name} differs"
+    compare_directories(tmp_path, expected_dir)
