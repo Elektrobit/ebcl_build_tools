0233f42a8e76405be92f3968ee4bd885710760a7
Joerg Vehlow <joerg.vehlow@aox.de>
2025-03-27 10:22:26 +0000

    rework credentials tests:
     * test_credential -> test_credentials: Credentials is not used
        in the singular form (at least for user/pass combination)
     * Move repository to its own folder. flat_repo is the repository for a different test case
     * Setup test repository using Makefile and in the isolated folder instead of polluting
       the workspace and depending on pre-exisitng scripts
     * Drop negative tests: They will never be executed anyway
     * Extend test to include a check if the credentials made it into the final filesystem (initrd and root)
     * Don't touch the cache: The tests should be run with a clean cache, but deleting the cache
        from the tests is not a good idea. (If required at all a temporary cache should be used)
     * Rename test package from ebcl-doc to cred-test-package: This makes it more clear, that it is a test package

--- tests/data/credentials_repo/repo_server.py
+++ tests/data/credentials_repo/repo_server.py
@@ -1,11 +1,12 @@
-import base64
 import argparse
-from functools import partial
-import os
+import base64
 import signal
 import sys
 import threading
+
+from functools import partial
 from http.server import SimpleHTTPRequestHandler, HTTPServer
+from typing import NoReturn
 
 
 """ global variable ok since used in pytest code only """
@@ -13,35 +14,39 @@ ebcl_auth_status = False
 
 
 class AuthHTTPRequestHandler(SimpleHTTPRequestHandler):
-    def __init__(self, *args, username, password, directory=None, **kwargs):
+    def __init__(self, *args, username: str, password: str, directory: str, **kwargs) -> None:
         self.username = username
         self.password = password
-        if directory is None:
-            directory = os.getcwd()
         super().__init__(*args, directory=directory, **kwargs)
 
-    def do_AUTHHEAD(self):
+    def do_AUTHHEAD(self) -> None:
         self.send_response(401)
         self.send_header('WWW-Authenticate', 'Basic realm="Secure Area"')
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-    def do_GET(self):
+    def do_GET(self) -> None:
         global ebcl_auth_status
-        auth_header = self.headers.get('Authorization')
-        if auth_header is None:
-            ebcl_auth_status = False
-            self.do_AUTHHEAD()
-            self.wfile.write(b'No auth header received')
-        elif self.authenticate(auth_header):
-            ebcl_auth_status = True
-            SimpleHTTPRequestHandler.do_GET(self)
-        else:
-            ebcl_auth_status = False
-            self.do_AUTHHEAD()
-            self.wfile.write(b'Invalid credentials')
 
-    def authenticate(self, auth_header):
+        # Protect only the dists directory
+        # This allows fetching the key (Release.key) from the server without authentication
+        if self.path.startswith("/dists/"):
+            auth_header = self.headers.get('Authorization')
+            if auth_header is None:
+                ebcl_auth_status = False
+                self.do_AUTHHEAD()
+                self.wfile.write(b'No auth header received')
+            elif self.authenticate(auth_header):
+                ebcl_auth_status = True
+                super().do_GET()
+            else:
+                ebcl_auth_status = False
+                self.do_AUTHHEAD()
+                self.wfile.write(b'Invalid credentials')
+        else:
+            super().do_GET()
+
+    def authenticate(self, auth_header) -> bool:
         encoded_credentials = auth_header.split(' ')[1]
         decoded_credentials = base64.b64decode(encoded_credentials).decode('utf-8')
         username, password = decoded_credentials.split(':')
@@ -49,27 +54,23 @@ class AuthHTTPRequestHandler(SimpleHTTPRequestHandler):
 
 
 class Server:
-    def __init__(self, port, username, password, directory):
-        self.port = port
-        self.username = username
-        self.password = password
+    def __init__(self, port: int, username: str, password: str, directory: str) -> None:
         self.directory = directory
-        self.httpd = None
-        self.server_thread = None
-
-    def start(self):
+        self.port = port
         handler = partial(
             AuthHTTPRequestHandler,
-            username=self.username,
-            password=self.password,
-            directory=self.directory
+            username=username,
+            password=password,
+            directory=directory
         )
         self.httpd = HTTPServer(('', self.port), handler)
-        print(f'Serving APT repository from {self.directory} on http://localhost:{self.port}')
         self.server_thread = threading.Thread(target=self.httpd.serve_forever)
+
+    def start(self) -> None:
+        print(f'Serving APT repository from {self.directory} on http://localhost:{self.port}')
         self.server_thread.start()
 
-    def stop(self):
+    def stop(self) -> None:
         if self.httpd:
             print("Stopping server...")
             self.httpd.shutdown()
@@ -77,23 +78,23 @@ class Server:
             self.server_thread.join()
             print("Server stopped.")
 
-    def get_last_auth_status(self):
+    def get_last_auth_status(self) -> bool:
         global ebcl_auth_status
         return ebcl_auth_status
 
 
-def signal_handler(signum, frame):
+def signal_handler(signum, frame) -> NoReturn:
     print(f"Received signal {signum}")
     server.stop()
     sys.exit(0)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Run an authenticated HTTPS server for APT repository')
+    parser = argparse.ArgumentParser(description='Run an authenticated HTTP server for APT repository')
     parser.add_argument('--port', type=int, default=8088, help='Port to run the server on')
-    parser.add_argument('--username', required=True, help='Username for authentication')
-    parser.add_argument('--password', required=True, help='Password for authentication')
-    parser.add_argument('--directory', required=True, help='Directory containing the APT repository')
+    parser.add_argument('--username', default="ebcl", help='Username for authentication')
+    parser.add_argument('--password', default="ebcl", help='Password for authentication')
+    parser.add_argument('--directory', default="repo", help='Directory containing the APT repository')
     args = parser.parse_args()
 
     server = Server(args.port, args.username, args.password, args.directory)
